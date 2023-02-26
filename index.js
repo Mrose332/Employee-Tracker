009c48a91d23ac3338dcc69fede5a40f753ab783
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-02-25 20:13:19 -0500

    commit changes to prompt questions

--- index.js
+++ index.js
@@ -25,15 +25,31 @@ const startQuestions = [
   message: "Please choose option?",
   choices:
       [
-        { name: 'View All Roles', value: 'vad' },
-        { name: 'View All Employees', value: 'vae' },
-        { name: 'View All Roles', value: 'var' },
-        { name: 'Add an Employee', value: 'add-employee' },
+        { name: 'View Roles', value: 'vad' },
+        { name: 'View Employees', value: 'vae' },
+        { name: 'View Deparments', value: 'var' },
+        { name: 'Done', value: 'var' },
       ]
   }
 ];
 
-const Department = [
+const RolesQ = [
+  {
+  type: "list",
+  name: "roles",
+  message: "Please choose Role options?",
+  choices: 
+      [
+      "View all Roles",
+      "Add a Role",
+      "Delete a Role",
+      "Done",    
+      ]
+  }
+];  
+
+
+const DepartmentQ = [
   {
   type: "list",
   name: "department",
@@ -48,15 +64,161 @@ const Department = [
   }
 ];    
 
-const addEmployeeQuestions = [{
-  type: 'input',
-  name: 'first_name',
-  message: 'What is the first name of the employee?'
-}, {
-  type: 'input',
-  name: 'last_name',
-  message: 'What is the last name of the employee?' 
-}];
+const EmployeesQ = [
+  {
+  type: "list",
+  name: "employees",
+  message: "Pleae choose Employee options?",
+  choices: 
+      [
+      "View all Employees",
+      "Add an Employee",
+      "Delete an Employee",
+      "Done",    
+      ]
+  }
+];    
 
 
 
+inquirer
+.prompt(startQuestions)
+.then(({ start }) => {
+ if(start === 'View Roles') {
+   inquirer.prompt(RolesQ)
+     .then(employeeAnswer => console.log(employeeAnswer))
+ }
+ else if(start === 'View Employees') {
+  inquirer.prompt(EmployeesQ)
+    .then(employeeAnswer => console.log(employeeAnswer))
+}
+else if(start === 'View Departments') {
+  inquirer.prompt(DepartmentsQ)
+    .then(employeeAnswer => console.log(employeeAnswer))
+}
+})
+if (err) {throw err;}
+      else {
+      startQuestions();
+    }
+
+
+
+    inquirer
+    .prompt(DepartmentQ)
+    .then(({ department }) => {
+     if(department === 'View all Departments') {
+       inquirer.prompt()
+         .then(employeeAnswer => console.log(employeeAnswer))
+     }
+     else if(department === 'Add a Department') {
+      inquirer.prompt(addDepartment)
+        .then(employeeAnswer => console.log(employeeAnswer))
+    }
+    else if(department === 'Delete a Department') {
+      inquirer.prompt()
+        .then(employeeAnswer => console.log(employeeAnswer))
+    }
+    })
+    if (err) {throw err;}
+          else {
+          startQuestions();
+        }
+
+
+        inquirer
+    .prompt(RolesQ)
+    .then(({ roles }) => {
+     if(roles === 'View all Roles') {
+       inquirer.prompt()
+         .then(employeeAnswer => console.log(employeeAnswer))
+     }
+     else if(roles === 'Add a Role') {
+      inquirer.prompt(addRole)
+        .then(employeeAnswer => console.log(employeeAnswer))
+    }
+    else if(roles === 'Delete a Role') {
+      inquirer.prompt()
+        .then(employeeAnswer => console.log(employeeAnswer))
+    }
+    })
+    if (err) {throw err;}
+          else {
+          startQuestions();
+        }
+
+
+        inquirer
+        .prompt(EmployeesQ)
+        .then(({ employees }) => {
+         if(employees === 'View all Employees') {
+           inquirer.prompt()
+             .then(employeeAnswer => console.log(employeeAnswer))
+         }
+         else if(employees=== 'Add an Employee') {
+          inquirer.prompt(addEmployee)
+            .then(employeeAnswer => console.log(employeeAnswer))
+        }
+        else if(employees === 'Delete an Employee') {
+          inquirer.prompt()
+            .then(employeeAnswer => console.log(employeeAnswer))
+        }
+        })
+        if (err) {throw err;}
+              else {
+              startQuestions();
+            } 
+
+
+            const addRole = [{
+              type: 'input',
+              name: 'newRole',
+              message: 'Enter name of new Role?',
+
+            }, {
+              type: 'input',
+              name: 'NewRoleSalary',
+              message: 'Enter the salary amount for the new role:',
+
+            }, {
+              type: 'input',
+              name: 'NewDepartmentId',
+              message: 'Enter the department ID for the new role:'
+
+             }
+            ]; 
+            
+            
+            const addDepartment = [{
+              type: 'input',
+              name: 'newDepartment',
+              message: 'Enter name of new department?'
+
+              
+            }
+            ]; 
+            
+            
+            const addEmployee = [{
+              type: 'input',
+              name: 'first_name',
+              message: 'What is the first name of the employee?'
+            }, {
+              type: 'input',
+              name: 'last_name',
+              message: 'What is the last name of the employee?'
+             
+            }, {
+              type: 'input',
+              name: 'NewRoleId',
+              message: 'Enter the new employee Id'
+
+            }, {
+              type: 'input',
+              name: 'NewManagerId',
+              message: "Please enter the manager's ID number:",
+
+             } 
+              
+            ];
+            
